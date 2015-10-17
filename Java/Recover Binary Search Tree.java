9ff9ee4e0825e612484605a3cf26d7c7ad9592e0
git-chongtianzs <chenliapplying@gmail.com>
2015-10-17 15:53:12 -0700

    Update Recover Binary Search Tree.java

--- Java/Recover Binary Search Tree.java
+++ Java/Recover Binary Search Tree.java
@@ -1,1 +1,36 @@
-
+//Morris traversal(inorder)
+public void recoverTree(TreeNode root){
+	TreeNode cur=root,tmp=null;
+	TreeNode fir=null,sec=null,pre=null;
+	while(cur!=null){
+		if(cur.left==null){
+			if(pre!=null&&pre.val>cur.val){
+				if(fir==null)
+					fir=pre;
+				sec=cur;
+			}
+			pre=cur;
+			cur=cur.right;
+		}else{
+			tmp=cur.left;
+			while(tmp.right!=null&&tmp.right!=cur)
+				tmp=tmp.right;
+			if(tmp.right==null){
+				tmp.right=cur;
+				cur=cur.left;
+			}else{
+				if(pre!=null&&pre.val>cur.val){
+					if(fir==null)
+						fir=pre;
+					sec=cur;
+				}
+				tmp.right=null;
+				pre=cur;
+				cur=cur.right;
+			}
+		}
+	}
+	int temp=fir.val;
+	fir.val=sec.val;
+	sec.val=temp;
+}
